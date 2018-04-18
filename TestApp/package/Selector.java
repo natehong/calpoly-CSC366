32449fb531b0e879c1ea9510c95cd59677457d10
delung <dereklung1996@hotmail.com>
2018-04-17 17:11:04 -0700

    added selectors for webpages

---
+++ TestApp/package/Selector.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.Serializable;
+import javax.inject.Named;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author stanchev
+ */
+@Named(value = "selector")
+@ManagedBean
+@SessionScoped
+
+public class Selector {
+    
+    private String startDate;
+    private String endDate;
+    private String viewChoice;
+    private String roomChoice;
+    
+    private String[] viewChoices = {"Pool", "Ocean"};
+    private String[] roomChoices = {"Single", "Double", "Queen", "King"};
+    
+    public String getStartDate() {
+        return startDate;
+    }
+    public void setStartDate(String choice) {
+        this.startDate = choice;
+    }
+    
+    public String getEndDate() {
+        return endDate;
+    }
+    
+    public void setEndDate(String choice) {
+        this.endDate = choice;
+    }
+    
+    public String getViewChoice() {
+        return viewChoice;
+    }
+    
+    public String[] getViewChoices() {
+        return viewChoices;
+    }
+    
+    public void setViewChoice(String choice) {
+        this.viewChoice = choice;
+    }
+    
+    public String getRoomChoice() {
+        return roomChoice;
+    }
+    public String[] getRoomChoices() {
+        return roomChoices;
+    }
+    
+    public void setRoomChoice(String choice) {
+        this.roomChoice = choice;
+    }
+    
+    public void transition() {
+        System.out.println(startDate);
+    }
+}
